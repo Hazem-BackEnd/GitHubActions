dad96e41abb1068ec7d09b25ab562c2f9435ae36
Hazem-BackEnd <dondeo329@gmail.com>
2025-10-18 00:46:56 +0300

    Edited for Testing Failure111

--- src/main/java/org/example/StringUtils.java
+++ src/main/java/org/example/StringUtils.java
@@ -3,7 +3,7 @@ package org.example;
 public class StringUtils {
 
     public static int getLength(String input) {
-        if (input == null) return 1;
+        if (input == null) return 0;
         return input.length();
     }
 
